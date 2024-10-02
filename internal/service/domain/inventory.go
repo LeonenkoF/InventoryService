ac18740fb69f0b5aa0ad6dfa27bc69b6463abcd5
Leonenko.Fedor <leonenko.f@yandex.ru>
2024-10-02 20:24:29 +0500

    fixing a bug

--- internal/service/domain/inventory.go
+++ internal/service/domain/inventory.go
@@ -26,14 +26,16 @@ func (is *InventoryService) GetInventoryList() ([]entity.Inventory, error) {
 }
 
 func (is *InventoryService) AddInventory(input entity.Inventory) error {
+
+	if len(input.Full_name) == 0 || len(input.Pc_id) == 0 || len(input.Inventory_num) == 0 || len(input.Invent_monitors) == 0 {
+		return fmt.Errorf("full_name or pc_id or Inventory_num or Invent_monitors must be filled out")
+	}
+
 	err := is.repo.AddInventory(input)
 
 	if err != nil {
 		return fmt.Errorf("InventoryService - AddInventory - is.repo.Getinventory error: %v", err)
 	}
 
-	if len(input.Full_name) == 0 || len(input.Pc_id) == 0 || len(input.Inventory_num) == 0 || len(input.Invent_monitors) == 0 {
-		return fmt.Errorf("full_name or pc_id or Inventory_num or Invent_monitors must be filled out")
-	}
 	return nil
 }
